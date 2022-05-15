24916a1831ce5e6bbf526a0fd03d04b138d9d2b2
Lomsha123 <lomsha@ce.iitr.ac.in>
2022-05-15 20:03:17 +0530

    Initial Commit

---
+++ components/home_page/food_card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const foodCart = () => {
+  return (
+    <div>
+      <div>
+        <h1>Veg Maggie</h1>
+        <h4>Rs. 30</h4>
+      </div>
+      <div>
+        <img src="../../public/home-page/firstImage.png" />
+        <button>Add</button>
+      </div>
+    </div>
+  );
+};
---
+++ pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { foodCart } from '../components/home_page/food_card';
+
+export const Home = () => {
+    return (
+        <div>
+            <foodCart/>
+            <foodCart/>
+            <foodCart/>
+        </div>
+    );
+};
